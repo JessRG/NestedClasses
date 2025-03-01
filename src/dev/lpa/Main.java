713d0c43ae6732a4b81f40d7398f650e067cfea3
Jesse <jesusgarza2511@gmail.com>
2025-03-01 15:11:23 -0600

    Add nested local class DecoratedEmployee in addPigLatinName method. Learning about scoping and effective-final variables.

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -50,5 +50,52 @@ public class Main {
             System.out.println(e);
         }
 
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+    }
+
+    // this function will accept a list of StoreEmployee items or any subtype
+    // of StoreEmployee
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        // local variable that is only assigned here
+        // this is effectively final unless reassigned somewhere in the method
+        String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee
+                implements Comparable<DecoratedEmployee> {
+
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>();
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+        newList.sort(null);
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee.originalInstance.getName() + " "
+                    + dEmployee.pigLatinName);
+        }
     }
 }
